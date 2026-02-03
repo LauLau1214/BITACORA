08dc4309313af96ffae60b3985356f774b8c5f9e
Laura Santiago <laurasantym1412@gmail.com>
2026-02-02 23:16:38 -0500

    Agrego adelanto de los ejercicios semana 1

---
+++ src/main/java/dosw/bitacora/semana1/streams.java
@@ -0,0 +1,40 @@
+//Ejercicios en clase
+//Streams y git Flow
+
+//EJERCICIO 1
+//Dada una lista de numeros enteros, necesitamos obtener una
+//nueva lista solo con los numeros pares mayores a diez
+//(REALIZADO EN CLASE)
+
+
+//EJERCICIO 2
+//Dada una lista de palabras, se requiere: Filtrar las palabras que
+//tengan mas de 4 caracteres, convertirlas en Mayusculas, ordenarlas
+//alfabeticamente y obtener la cantidad total de palabras resultantes
+//(REALIZADO EN CLASE)
+
+
+//EJERCICIO 3
+//Dada una lista de usuarios, Filtra unicamente los usuarios activos, obten
+//una lista con los nombres en mayuscula y ordena alfabeticamente el resultado,
+//el objeto usuario cuenta con los atributos:
+//identificador - name - age - Active
+
+
+
+//EJERCICIO 4
+//Dado un listado de Usuarios y utilizando los mismo atributos anteriores, filtrar
+//las personas mayores de edad y obtener sus nombres. (NUEVO)
+
+
+//EJERCICIO 5
+//Dada una lista de transacciones bancarias representadas por objetos:
+//class Transaction {
+//    String id;
+//    double amount;
+//    boolean approved;
+//}
+//Se requiere procesar la lista usando Streams para:
+//Usar peek para ver cada transacción procesada (Utilizar System.out.println para ver la transaccion)
+//Verificar si existe al menos una transacción no aprobada
+//Retornar true o false indicando si el lote de transacciones es válido.
